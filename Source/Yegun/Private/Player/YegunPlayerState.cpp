38f159b8fc873b1da445ec37cf0ed7f2c47f4bb1
Chukwuyenum Opone <oponechukwuyenum@gmail.com>
2023-12-14 18:16:26 +0000

    Player State Added

---
+++ Source/Yegun/Private/Player/YegunPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Yenum dot dev
+
+
+#include "Player/YegunPlayerState.h"
+
+AYegunPlayerState::AYegunPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Yegun/Public/Player/YegunPlayerState.h
@@ -0,0 +1,18 @@
+// Copyright Yenum dot dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "YegunPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YEGUN_API AYegunPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AYegunPlayerState();
+};
